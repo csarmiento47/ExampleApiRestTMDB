1605f053e2494340ca202a2c037c6cf9393f8dca
Christian Sarmiento Zampillo <christian.sarmiento@gmail.com>
2024-11-21 21:36:06 -0300

    Update Constants.kt

--- app/src/main/java/com/inforcap/exampleapiresttmdb/core/Constants.kt
+++ app/src/main/java/com/inforcap/exampleapiresttmdb/core/Constants.kt
@@ -2,7 +2,7 @@ package com.inforcap.exampleapiresttmdb.core
 
 object Constants {
 
-    const val API_KEY = "fa625d58ec8ff215eb2eb7cdb472d218"
+    const val API_KEY = "PUT-YOUR-APIKEY-HERE"
     const val API_URL = "https://api.themoviedb.org/3/movie/"
     const val API_URL_IMG = "https://image.tmdb.org/t/p/w500/"
     const val IMG_WIDTH = 400
